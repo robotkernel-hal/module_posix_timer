d0fba208d09c5b3171bd7867b719971640ae78dc
Robert Burger <robert.burger@dlr.de>
2021-12-03 06:28:43 +0100

    CHANGE: switched to new template and python_require

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,14 @@
-from conans import tools, python_requires
+from conans import ConanFile, tools
+import os
 
-base = python_requires("conan_template/[~=5]@robotkernel/stable")
+class MainProject(ConanFile):
+    python_requires = "conan_template_ln_generator/[~=5 >=5.0.7]@robotkernel/stable"
+    python_requires_extend = "conan_template_ln_generator.RobotkernelLNGeneratorConanFile"
 
-class MainProject(base.RobotkernelConanFile):
     name = "module_posix_timer"
     description = "module_posix_timer is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-
-    def requirements(self):
-        self.requires("robotkernel/[~=5]@robotkernel/stable")
-        self.requires("service_provider_process_data_inspection/[~=5]@robotkernel/stable")
+    requires = [    
+        "robotkernel/[~=5]@robotkernel/stable", 
+        "service_provider_process_data_inspection/[~=5]@robotkernel/stable" ]
 
