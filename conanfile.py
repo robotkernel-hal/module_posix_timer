83b163c4049f2711709e0048391592f96869e7cd
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2023-11-14 10:46:07 +0100

    chore: black format

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 import os
 
+
 class MainProject(ConanFile):
     python_requires = "conan_template/[~=5]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
@@ -8,7 +9,4 @@ class MainProject(ConanFile):
     name = "module_posix_timer"
     description = "module_posix_timer is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"]
-    requires = [    
-        "robotkernel/[~=5]@robotkernel/stable", 
-        "service_provider_process_data_inspection/[~=5]@robotkernel/stable" ]
-
+    requires = ["robotkernel/[~=5]@robotkernel/stable", "service_provider_process_data_inspection/[~=5]@robotkernel/stable"]
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,6 +4,7 @@ import shutil
 from conan import ConanFile
 from conan.tools.build import can_run
 
+
 class TestTestConan(ConanFile):
     test_type = "explicit"
     settings = "os", "compiler", "build_type", "arch"
@@ -15,10 +16,9 @@ class TestTestConan(ConanFile):
 
     def test(self):
         rkc_path = os.path.join(self.source_folder, "mod_test.rkc")
-        shutil.copy(rkc_path, 'mod_test.rkc')
+        shutil.copy(rkc_path, "mod_test.rkc")
 
         if can_run(self):
             self.run("robotkernel --test-run --config .%smod_test.rkc" % os.sep, env="conanrun")
         else:
             self.output.warn("Skipping run cross built package")
-
