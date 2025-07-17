2326df9add546ddc5342f83f2252045cfc163c57
Robert Burger <robert.burger@dlr.de>
2025-07-17 12:20:10 +0200

    fix: AC_INIT generation

--- conanfile.py
+++ conanfile.py
@@ -11,3 +11,7 @@ class MainProject(ConanFile):
     description = "module_posix_timer is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"]
     requires = ["robotkernel/[~6]@robotkernel/unstable", "service_provider_process_data_inspection/[~6]@robotkernel/unstable"]
+    
+    def source(self):
+        self.run(f"sed 's/AC_INIT(.*/AC_INIT([robotkernel], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+
