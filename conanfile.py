166d857f632c59d20af2ee9588809aa0c2038081
Robert Burger <robert.burger@dlr.de>
2026-02-12 08:36:48 +0100

    change: switch to robotkernel unstable

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,10 @@ class MainProject(ConanFile):
     name = "module_posix_timer"
     description = "module_posix_timer is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"]
-    requires = ["robotkernel/[~6]@robotkernel/unstable", "service_provider_process_data_inspection/[~6]@robotkernel/unstable"]
-    
+    requires = [
+        "robotkernel/6.0.0-yaml-service@robotkernel/unstable",
+        "service_provider_process_data_inspection/6.0.0-yaml-service@robotkernel/unstable" ]
+
     def source(self):
         self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
