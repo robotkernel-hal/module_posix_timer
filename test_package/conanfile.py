160352a35781e5b67f5f7753fe97cb041a93c886
Robert Burger <robert.burger@dlr.de>
2026-03-16 10:52:01 +0100

    fix: copy with same src und dest

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,12 +13,8 @@ class TestTestConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def build(self):
-        copy(self, "mod_test.rkc", self.recipe_folder, self.package_folder)
-
     def test(self):
         if can_run(self):
-            self.run("pwd")
             self.run("robotkernel --test-run --config %s" % os.path.join(self.package_folder, "mod_test.rkc"), env="conanrun")
         else:
             self.output.warn("Skipping run cross built package")
