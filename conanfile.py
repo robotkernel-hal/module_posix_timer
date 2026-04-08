44498eb779e698a2fc3c94b07e2325558d7a50cd
Robert Burger <robert.burger@dlr.de>
2026-04-08 15:59:37 +0200

    fix: build system with conan_template 6

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,8 @@ from conan import ConanFile, conan_version
 from conan.tools.scm import Version
 import os
 
-
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~5]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_posix_timer"
@@ -16,7 +15,4 @@ class MainProject(ConanFile):
     requires = [
         "robotkernel/[~6]@robotkernel/unstable",
         "service_provider_process_data_inspection/[~6]@robotkernel/unstable" ]
-
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
