ddf9b8a10ca6c3e58aa43ff9f9f891a6b42e5d98
Robert Burger <robert.burger@dlr.de>
2026-05-19 06:46:35 +0200

    change: deps version to stable

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,16 @@ from conan.tools.scm import Version
 import os
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_posix_timer"
     description = "module_posix_timer is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"]
     tool_requires = [
-        "robotkernel_generator/[~6]@robotkernel/unstable" ]
+        "robotkernel_generator/[~6]@robotkernel/stable" ]
 
     requires = [
-        "robotkernel/[~6]@robotkernel/unstable",
-        "service_provider_process_data_inspection/[~6]@robotkernel/unstable" ]
+        "robotkernel/[~6]@robotkernel/stable",
+        "service_provider_process_data_inspection/[~6]@robotkernel/stable" ]
 
